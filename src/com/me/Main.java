29d4f68259d2c8253aff8ff92ceba0256a0e1fd0
patrickatcw <pj8392@yahoo.com>
2018-03-26 09:45:56 -0400

    work done in main

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -21,5 +21,84 @@ simulate getting values from a file - this allows you to type as many values as 
 requires, and returns an arraylist
  */
 
+/*
+https://docs.oracle.com/javase/tutorial/java/concepts/interface.html
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-}
+
+    public static void main(String[] args) {
+
+        //step 15 creating instance
+        Player patrick = new Player("Patrick", 10, 15);
+        System.out.println(patrick.toString());
+        savedObject(patrick);
+
+        //step 16 changes
+        patrick.setHitpoints(8);
+        System.out.println(patrick);
+        patrick.setWeapon("Stormbringer");
+        savedObject(patrick);
+        loadObject(patrick);
+        System.out.println(patrick);
+
+        //step 17 run
+
+    }
+
+        //input from lesson the read values method outside of the main
+        public static ArrayList<String> readValues () {
+            ArrayList<String> values = new ArrayList<String>();
+
+            Scanner scanner = new Scanner(System.in);
+            boolean quit = false;
+            int index = 0;
+            System.out.println("Choose\n" +
+                    "1 to enter a string\n" +
+                    "0 to quit");
+
+            while (!quit) {
+                System.out.println("Choose an option: ");
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+                switch (choice) {
+                    case 0:
+                        quit = true;
+                        break;
+                    case 1:
+                        System.out.println("Enter a string: ");
+                        String stringInput = scanner.nextLine();
+                        values.add(index, stringInput);
+                        index++;
+                        break;
+                }
+
+            }
+            return values;
+
+        }
+
+        //step 13 method
+        public static void savedObject (ISaveable objectToSave){
+
+            for (int i = 0; i < objectToSave.write().size(); i++) {
+
+                System.out.println("Saving " + objectToSave.write().get(i) + " to storage device");
+
+            }
+
+        }
+
+        //step 14 method for load object
+        public static void loadObject (ISaveable objectToLoad){
+            ArrayList<String> values = readValues();
+            objectToLoad.read(values);
+
+
+        }
+    }
+
+
