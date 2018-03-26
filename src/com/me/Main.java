b48ec14d461b27a84ef17a19fd17f4bd8a8321ea
patrickatcw <pj8392@yahoo.com>
2018-03-26 09:29:20 -0400

    added directions for program

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -1,4 +1,25 @@
 package com.me;
 
+//directions
+/*
+- create a simple interface (tip: name it Saveable or ISaveable)
+- that interface allows an object to be saved to some sort of storage medium. (tip: use arraylist)
+- the exact type of medium is not known to the interface (nor to the classes that implement it).
+- the interface will just specify 2 methods, one to return an arraylist of values to be saved
+and the other to populate the object's fields from an arraylist (parameter).
+- create a few sample classes that implement your Saveable interface (we've used tthe idea of a game,
+with players and monsters, but you can create any type of classes that you want).
+- override the toString()method for each of your classes so that they can be easily printed to enable
+the program to be tested easier.
+- in main, write a method that takes an object that implements the interface as a parameter and
+"saves" the values, ex. calls the method defined in the interface.
+- we haven't covered I/O yet, so your method should just print the values to the screen.
+- also in the main class, write a method that retores the values to a saveable object
+ex. calls the mthod from the interface for populating fields (see above)
+- again we are not going to use java file I/O; instead use the readValues() method below to
+simulate getting values from a file - this allows you to type as many values as your class
+requires, and returns an arraylist
+ */
+
 public class Main {
 }
